10718e3c32781b28e7f0843464494d12e5fbae42
Gilvam Mourão <gilvamm@gmail.com>
2021-05-18 19:47:15 -0300

    feat: adicionado módulo categoria

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'categories', loadChildren: () => import('./pages/categories/categories.module').then(i => i.CategoriesModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
